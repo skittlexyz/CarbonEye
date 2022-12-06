2640a05f411c43618b20a1748136855a23704618
Moisés Corrêa <54242235+skittlexyz@users.noreply.github.com>
2022-12-06 09:43:06 -0300

    Delete webserver/randomppm directory

--- webserver/randomppm/credentials.h
+++
@@ -1,5 +0,0 @@
-// const char NAME[] PROGMEM = R"=====(mms260131)=====";
-// const char PASS[] PROGMEM = R"=====(sumumo01)=====";
-
-const char NAME[] PROGMEM = R"=====(FAB LAB)=====";
-const char PASS[] PROGMEM = R"=====(fl4ce0ss3@camp)=====";
