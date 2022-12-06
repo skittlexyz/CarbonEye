61b7a3a59247e4be6bda2b573def1187cf60a0f3
Moisés Corrêa <54242235+skittlexyz@users.noreply.github.com>
2022-12-06 09:44:26 -0300

    Update credentials.h

--- webserver/credentials.h
+++ webserver/credentials.h
@@ -1,5 +1,2 @@
-// const char NAME[] PROGMEM = R"=====(mms260131)=====";
-// const char PASS[] PROGMEM = R"=====(sumumo01)=====";
-
-const char NAME[] PROGMEM = R"=====(FAB LAB)=====";
-const char PASS[] PROGMEM = R"=====(fl4ce0ss3@camp)=====";
+const char NAME[] PROGMEM = R"=====(YOUR SSID GOES HERE)=====";
+const char PASS[] PROGMEM = R"=====(YOUR PASSWORD GOES HERE)=====";
